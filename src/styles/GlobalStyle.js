bc5202982cb07c2a23e149eccb210b38c5d20bf8
Mick Rippon <me@mickrippon.com>
2019-12-26 20:26:40 +1100

    Favicons and stuff

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,12 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css?family=Bebas+Neue&display=swap');
+
 @import url('https://fonts.googleapis.com/css?family=Comfortaa&display=swap');
   body {
   font-family: 'Comfortaa', cursive;
   color: #444;
-  font-size: 12px;
+  font-size: 13px;
   //letter-spacing: 1px;
     background: white;
   }
